0d47b5f8118260814416b61f0fdb36f1392bb7f4
sahar-molaazadeh <mollazadeh.sahar@hasti.co>
2024-09-30 18:32:32 +0330

    feat/ add styles

--- src/components/base/BaseFormBuilder.vue
+++ src/components/base/BaseFormBuilder.vue
@@ -1,58 +1,66 @@
 <template>
-    <form @submit.prevent="">
-        <template v-for="element in elements" :key="element.name">
-            <component v-if="shouldRenderElement(element)" :modelValue="modelValue[element.name]" :element="element"
-                :is="getComponent(element.type)"
-                @update:modelValue="(value: any) => updateValue(element.name, value)" />
-
-        </template>
-    </form>
+  <form @submit.prevent="">
+    <template v-for="element in elements" :key="element.name">
+      <component
+        v-if="shouldRenderElement(element)"
+        :modelValue="modelValue[element.name]"
+        :element="element"
+        :is="getComponent(element.type)"
+        @update:modelValue="(value: any) => updateValue(element.name, value)"
+      />
+    </template>
+  </form>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue';
-import type { InputElement } from '@/types';
-import { InputTypeEnum } from '@/types';
-import BaseText from './BaseText.vue';
-import BaseDropDown from './BaseDropDown.vue';
-import BaseCheckbox from './BaseCheckbox.vue';
-import BaseCheckboxGroup from './BaseCheckboxGroup.vue';
-import BaseRange from './BaseRange.vue';
+import { defineProps } from 'vue'
+import type { InputElement } from '@/types'
+import { InputTypeEnum } from '@/types'
+import BaseText from './BaseText.vue'
+import BaseDropDown from './BaseDropDown.vue'
+import BaseCheckbox from './BaseCheckbox.vue'
+import BaseCheckboxGroup from './BaseCheckboxGroup.vue'
+import BaseRange from './BaseRange.vue'
 
 const props = defineProps<{
-    elements: InputElement[];
-    modelValue: { [key: string]: any };
-}>();
-const emit = defineEmits(['update:modelValue']);
+  elements: InputElement[]
+  modelValue: { [key: string]: any }
+}>()
+const emit = defineEmits(['update:modelValue'])
 
 const shouldRenderElement = (element: InputElement) => {
-    if (!element.parent) return true;
-    const parentValue = props.modelValue[element.parent];
-    
-    if (Array.isArray(parentValue)) {
-        return parentValue.includes(element.name);
-    }
+  if (!element.parent) return true
+  const parentValue = props.modelValue[element.parent]
 
-    if (element.type === InputTypeEnum.Range) {
-        return !!parentValue;
-    }
-    
-    return parentValue && (element.name === parentValue || parentValue === true);
-};
+  if (Array.isArray(parentValue)) {
+    return parentValue.includes(element.name)
+  }
+
+  if (element.type === InputTypeEnum.Range) {
+    return !!parentValue
+  }
+
+  return parentValue && (element.name === parentValue || parentValue === true)
+}
 
 const getComponent = (type: string) => {
-    switch (type) {
-        case 'text': return BaseText;
-        case 'dropdown': return BaseDropDown;
-        case 'checkbox': return BaseCheckbox;
-        case 'checkbox-group': return BaseCheckboxGroup;
-        case 'range': return BaseRange;
-        default: return null;
-    }
-};
+  switch (type) {
+    case 'text':
+      return BaseText
+    case 'dropdown':
+      return BaseDropDown
+    case 'checkbox':
+      return BaseCheckbox
+    case 'checkbox-group':
+      return BaseCheckboxGroup
+    case 'range':
+      return BaseRange
+    default:
+      return null
+  }
+}
 
 const updateValue = (name: string, value: any) => {
-    emit('update:modelValue', { ...props.modelValue, [name]: value })
-};
-
+  emit('update:modelValue', { ...props.modelValue, [name]: value })
+}
 </script>
--- src/composables/useUrlSync.ts
+++ src/composables/useUrlSync.ts
@@ -1,70 +1,83 @@
-import { ref, watch, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import type { InputElement } from '@/types';
-import { InputTypeEnum } from '@/types';
-import { buildCustomQuery, parseCustomQuery } from '@/utils';
+import { ref, watch, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import type { InputElement } from '@/types'
+import { InputTypeEnum } from '@/types'
+import { buildCustomQuery, parseCustomQuery } from '@/utils'
 
+//TODO: I should do it more better.
+// This composable function synchronizes form data with URL parameters
 export function useUrlSync(elements: InputElement[]) {
-    const route = useRoute();
-  
-    const formData = ref<{ [key: string]: any }>({});
-  
-    onMounted(() => {
-      const query = route.fullPath.includes('?') ? route.fullPath.split('?')[1] : '';
-      if (query) {
-        const parsedParams = parseCustomQuery(query);
-  
-        Object.keys(parsedParams).forEach((key) => {
-          const element = elements.find(el => el.name === key);
-          if (element) {
-            if (element.type === InputTypeEnum.CheckboxGroup) {
-              formData.value[key] = Array.isArray(parsedParams[key])? parsedParams[key] : [parsedParams[key]];
-            } else {
-              formData.value[key] = parsedParams[key];
-            }
-          }
-        });
-      }
-    });
-  
-    watch(formData, (newFormData) => {
-      const updatedParams: { [key: string]: string | string[] | undefined } = {...formData.value};
-        console.log(newFormData);
-        
-      Object.keys(newFormData).forEach((key) => {
-        const value = newFormData[key];
-        const element = elements.find(el => el.name === key);
-  
-        if (value && element) {
+  const route = useRoute()
+
+  // Reactive object to store form data
+  const formData = ref<{ [key: string]: any }>({})
+  // Initialize form data from URL parameters when component is mounted
+  onMounted(() => {
+    const query = route.fullPath.includes('?') ? route.fullPath.split('?')[1] : ''
+    if (query) {
+      const parsedParams = parseCustomQuery(query)
+
+      //Papulate formData with paarsed params
+      Object.keys(parsedParams).forEach((key) => {
+        const element = elements.find((el) => el.name === key)
+        if (element) {
           if (element.type === InputTypeEnum.CheckboxGroup) {
-            updatedParams[key] = value;
+            formData.value[key] = Array.isArray(parsedParams[key])
+              ? parsedParams[key]
+              : [parsedParams[key]]
           } else {
-            updatedParams[key] = String(value);
+            formData.value[key] = parsedParams[key]
           }
         }
-      });
-  
-      const customQuery: string = buildCustomQuery(updatedParams);
-      console.log(customQuery);
-      
-      window.history.replaceState(null, '', customQuery);
-    }, { deep: true });
-  
-    function clearField(fieldName: string) {
-      const element = elements.find(el => el.name === fieldName);
-      if (!element) return;
-  
-      formData.value[fieldName] = undefined;
-  
-      if (element.children) {
-        element.children.forEach(childName => {
-          formData.value[childName] = undefined;
-        });
-      }
+      })
     }
-  
-    return {
-      formData,
-      clearField,
-    };
+  })
+
+  // Watch for changes in formData and update URL accordingly
+  watch(
+    formData,
+    (newFormData) => {
+      const updatedParams: { [key: string]: string | string[] | undefined } = { ...formData.value }
+      console.log(newFormData)
+
+      // Process each form field
+      Object.keys(newFormData).forEach((key) => {
+        const value = newFormData[key]
+        const element = elements.find((el) => el.name === key)
+
+        if (value && element) {
+          if (element.type === InputTypeEnum.CheckboxGroup) {
+            updatedParams[key] = value
+          } else {
+            updatedParams[key] = String(value)
+          }
+        }
+      })
+
+      // Build and update URL with new parameters
+      const customQuery: string = buildCustomQuery(updatedParams)
+      console.log(customQuery, '++++')
+      window.history.replaceState(null, '', customQuery)
+    },
+    { deep: true }
+  )
+
+  // Function to clear a specific field and its children (if any)
+  function clearField(fieldName: string) {
+    const element = elements.find((el) => el.name === fieldName)
+    if (!element) return
+
+    formData.value[fieldName] = undefined
+
+    if (element.children) {
+      element.children.forEach((childName) => {
+        formData.value[childName] = undefined
+      })
+    }
   }
+
+  return {
+    formData,
+    clearField
+  }
+}
